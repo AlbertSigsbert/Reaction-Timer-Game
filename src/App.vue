f69f3ced0fd4985cb589555be494c24a0975c15f
Albert <ishengomaalbert@gmail.com>
2023-03-14 12:45:25 +0300

    Lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>Vue Reaction Timer</h1>
+<button @click="start" :disabled="isPlaying">play</button>
+<Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block
+  },
+  data(){
+    return{
+      isPlaying:false,
+      delay:null
+    }
+  },
+  methods:{
+    start(){
+     this.delay = 2000 + Math.random() * 5000
+     this.isPlaying = true
+    }
   }
+
 }
 </script>
 
@@ -20,7 +35,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="block" v-if="showBlock">
+    Click Me!
+  </div>
+</template>
+
+<script>
+export default {
+ props:['delay'],
+ data(){
+    return {
+        showBlock:false
+    }
+ },
+ mounted(){
+    console.log('Component Mounted');
+    setTimeout(() => {
+        this.showBlock = true
+        console.log(this.delay);
+    }, this.delay)
+ },
+
+ updated(){
+    console.log('Component updated');
+ },
+ unmounted(){
+    console.log('Component Unmounted');
+ }
+}
+</script>
+
+<style>
+.block{
+    width: 400px;
+    border-radius: 20px;
+    background: #257b7e;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>
